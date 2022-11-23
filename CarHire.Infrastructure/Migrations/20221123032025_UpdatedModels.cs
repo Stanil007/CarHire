55d0627fc530d3832b62e9776952c4ea637e5216
Stanil Kalaydzhiev <stanil107@gmail.com>
2022-11-23 03:51:42 +0000

    updated models

---
+++ CarHire.Infrastructure/Migrations/20221123032025_UpdatedModels.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarHire.Infrastructure.Migrations
+{
+    public partial class UpdatedModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PricePerDay",
+                table: "Contracts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHired",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerDay",
+                table: "Vehicles",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "IsHired",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerDay",
+                table: "Vehicles");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerDay",
+                table: "Contracts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
